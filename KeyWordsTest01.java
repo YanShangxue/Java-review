6b1e3239fd486dd9f3f83939c4cc09cdf2a0057c
YanShangxue <81284984+YanShangxue@users.noreply.github.com>
2021-12-04 09:26:25 +0800

    Add files via upload

---
+++ KeyWordsTest01.java
@@ -0,0 +1,13 @@
+/*
+    什么是关键字？
+    -SUM在开发JAVA语言的时候，提前制定好的一些具有特定含义的字符序列。
+    -在语言中具有特殊含义的单词，这些单词构成了java程序的骨架，这些单词
+    是需要记住的，不能随意编写，包括大小写。
+    -关键字在JAVA中全部是小写
+*/
+public class KeyWordsTest01 
+{
+    public static void main(String[] args) {
+        
+    }    
+}
