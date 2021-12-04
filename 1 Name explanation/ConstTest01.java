1faf9874bb277d0ff07a92eeac03c4c95a97a0fb
YanShangxue <81284984+YanShangxue@users.noreply.github.com>
2021-12-04 09:31:53 +0800

    Create ConstTest01.java

---
+++ 1 Name explanation/ConstTest01.java
@@ -0,0 +1,16 @@
+/*
+    * 字面值就是数据
+    * 数据在现实世界中是分门别类的，所以数据在计算机编程语言中也是有类型的
+    eg:
+        -10、100
+        -"abc"
+        -'a'
+        -ture、false
+
+*/
+public class ConstTest01 
+{
+    public static void main(String[] args) {
+        
+    }    
+}
