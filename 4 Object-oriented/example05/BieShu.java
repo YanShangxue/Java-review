4cc6a6de1472c9914877a91c9b469e44684552f2
YanShangxue <81284984+YanShangxue@users.noreply.github.com>
2021-12-14 20:33:36 +0800

    Add files via upload

---
+++ 4 Object-oriented/example05/BieShu.java
@@ -0,0 +1,12 @@
+//别墅类
+public class BieShu {
+    //面积
+    double mianJi;
+
+    //主人
+    //变量/引用
+    //关联关系：A has a B关系【A对象中有B对象的引用】
+    //BieShu对象含有Ren对象的引用
+    Ren zhuren=new Ren();
+    
+}
---
+++ 4 Object-oriented/example05/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    //属性【成员变量之，实例变量】
+    
+    //总价
+    //基本数据类型
+    double zongJia;
+
+    //品牌
+    //引用数据类型
+    String pinPai;
+
+    //颜色
+    //引用数据类型
+    String yanSe;
+    
+    //号牌
+    //引用数据类型
+    String haoPai;
+}
